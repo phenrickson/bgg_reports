df21ab70c3335e96f5e94c3dbf883facdf98c6d4
Phil Henrickson <phil.henrickson@gmail.com>
2022-03-10 16:34:23 -0600

    hopefully fixed the issue with the missing toc

--- scripts/run_individual_notebook.R
+++ scripts/run_individual_notebook.R
@@ -13,7 +13,8 @@
 library(tidyverse)
 library(foreach)
 
-user_list = c("Watch%20It%20Played")
+user_list = c("mrbananagrabber")
+#user_list = c("Watch%20It%20Played")
 
 # user_list = c("rahdo",
 #               "Quinns",
@@ -39,11 +40,12 @@ user_list = c("Watch%20It%20Played")
 
 # run through
 foreach(i=1:length(user_list)) %do% {
-        rmarkdown::render(here::here("notebooks", "predicting_user_collections.Rmd"), 
+        
+        rmarkdown::render(here::here("predicting_user_collections.Rmd"), 
                           params = list(username = user_list[i],
                                         end_training_year = 2020),
-                          output_file =  paste(gsub("\\%20", "", user_list[i]), 2020, sep="_"),
-                          output_dir = here::here("user_reports"))
+                          output_file =  paste(gsub("\\%20", "", user_list[i]), 2020, sep="_"))
+#                          output_dir = here::here())
 }
 
 rm(list=ls())
