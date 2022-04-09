d7077e8805e36d2fefac6851d299222d0fbf038a
Phil Henrickson <phil.henrickson@gmail.com>
2022-04-09 09:19:03 -0500

    fixes

---
+++ functions/usersrated_col_func.R
@@ -0,0 +1,23 @@
+usersrated_col_func <-
+        function(x) {
+                breaks = c(0,
+                           100,
+                           500,
+                           1000,
+                           1500,
+                           2000,
+                           5000,
+                           10000,
+                           25000,
+                           50000)
+                
+                colorRamp=colorRampPalette(c("white", "deepskyblue1"))
+                col_palette <- colorRamp(length(breaks))
+                mycut <- cut(x, 
+                             breaks = breaks,
+                             include.lowest = TRUE, 
+                             right=F,
+                             label = FALSE)
+                col_palette[mycut]
+                
+        }
