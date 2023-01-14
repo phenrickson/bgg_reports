26c820062f0b5008a8a532aeaf9d9122bfc443e9
Phil Henrickson <phil.henrickson@gmail.com>
2023-01-14 16:10:44 -0600

    adding script for pulling analysis tables

--- src/data/pull_analysis_games_tables.R
+++ src/data/pull_analysis_games_tables.R
@@ -9,6 +9,7 @@ message("querying tables from gcp for analysis...")
 suppressPackageStartupMessages(
         {
                 require(tidyverse)
+                require(tidymodels)
         }
 )
 
@@ -18,7 +19,7 @@ tidymodels_prefer()
 
 # connect to gcp ----------------------------------------------------------
 
-source(here::here("src", "functions", "connect_to_gcp.R"))
+source(here::here("src", "helpers", "connect_to_gcp.R"))
 
 
 # download tables ---------------------------------------------------------------
@@ -47,6 +48,11 @@ game_playercounts =
         bq_table_download(bq_project_query(PROJECT_ID,
                                            'SELECT * FROM bgg.api_game_playercounts'))
 
+# get descriptions
+game_descriptions = 
+        bq_table_download(bq_project_query(PROJECT_ID,
+                                           'SELECT * FROM bgg.api_game_descriptions'))
+
 # create additional tables ------------------------------------------------
 
 message("creating additional tables..")
@@ -56,26 +62,40 @@ unreleased_games =
         game_links %>%
         filter(value == 'Admin: Unreleased Games') %>%
         transmute(
-                game_id, 
+                type,
                 value,
-                family_id = id,
-                family = type,
+                id,
+                game_id,
                 load_ts
         )
 
-# games with description issues/flags/issues
-problem_games = game_links %>%
-        # anything flagged with Admin:
-        filter(grepl("Admin:", value)) %>%
-        # remove upcoming releases
+# games to drop from from _all_ analysis
+# games that are expansions for base games/fan expansions
+# games flagged with admin (excluding unreleased games)
+# games that are missing on yearpublished
+drop_games = 
+        # add in yearpublished and numweights for filtering
+        game_links %>%
+        left_join(.,
+                  analysis_games %>%
+                          select(game_id, name, yearpublished) %>%
+                          distinct,
+                  by = c("game_id")) %>%
+        # remove upcoming releases from this set
         filter(value != 'Admin: Unreleased Games') %>%
-        transmute(
-                game_id, 
-                value,
-                family_id = id,
-                family = type,
-                load_ts
-        )
+        filter(
+                (value %in% 
+                       c('Expansion for Base-game',
+                         'Fan Expansion',
+                         '(Looking for a publisher)')
+                )
+                | grepl("Admin:", value) 
+                | is.na(yearpublished) 
+                | is.na(name) 
+        ) %>%
+        # big box kickstarter 
+        select(game_id, name) %>%
+        unique
 
 # game compilations aka big boxes
 game_compilations =
@@ -83,8 +103,8 @@ game_compilations =
         filter(type == 'compilation') %>%
         transmute(
                 type,
-                compilation_name = value,
-                compilation_game_id = id,
+                name = value,
+                id = id,
                 game_id,
                 load_ts)
 
@@ -94,8 +114,8 @@ game_implementations =
         filter(type == 'implementation') %>%
         transmute(
                 type,
-                implementation_name = value,
-                implementation_game_id = id,
+                name = value,
+                id = id,
                 game_id,
                 load_ts)
 
@@ -104,8 +124,8 @@ game_categories =
         game_links %>%
         filter(type == 'category') %>%
         transmute(type,
-                  category_value = value,
-                  category_id = id,
+                  value = value,
+                  id = id,
                   game_id,
                   load_ts)
 
@@ -121,7 +141,8 @@ game_families =
         transmute(type,
                   family_type,
                   family_value,
-                  family_id = id,
+                  value = paste(family_type, family_value),
+                  id,
                   game_id,
                   load_ts)
 
@@ -130,8 +151,8 @@ game_designers =
         game_links %>%
         filter(type == 'designer') %>%
         transmute(type,
-                  designer_value = value,
-                  designer_id = id,
+                  value,
+                  id = id,
                   game_id,
                   load_ts)
 
@@ -140,21 +161,51 @@ game_publishers =
         game_links %>%
         filter(type == 'publisher') %>%
         transmute(type,
-                  publisher_value = value,
-                  publisher_id = id,
+                  value,
+                  id = id,
                   game_id,
                   load_ts)
 
+# game mechanics
+game_mechanics = 
+        game_links %>%
+        filter(type == 'mechanic') %>%
+        transmute(type,
+                  value,
+                  id = id,
+                  game_id,
+                  load_ts)
+
+# game artists
+game_artists = 
+        game_links %>%
+        filter(type == 'artist') %>%
+        transmute(type,
+                  value,
+                  id = id,
+                  game_id,
+                  load_ts)
+
+# save a copy of these tables for local use as an .Rdata object
+message("saving a local copy...")
+save(analysis_games,
+     unreleased_games,
+     drop_games,
+     game_links,
+     game_descriptions,
+     game_categories,
+     game_compilations,
+     game_designers,
+     game_families,
+     game_implementations,
+     game_playercounts,
+     game_publishers,
+     game_artists,
+     game_mechanics,
+     file = here::here("data", "local", "analysis_games_tables.Rdata"))
+
 message("done.")
 
-# # remove unreleased and problem games
-# tidy_games = 
-#         analysis_games %>%
-#         # remove unreleased and problem games
-#         filter(!(game_id %in% c(
-#                 unreleased_games$game_id,
-#                 problem_games$game_id))
-#         )
 
 
 
