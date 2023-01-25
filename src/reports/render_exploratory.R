b26d541ecc0ae3aabf0da02c2323b2251023afb6
Phil Henrickson <phil.henrickson@gmail.com>
2023-01-24 21:19:15 -0600

    render exploratory reports via rmarkdown

--- src/reports/render_exploratory.R
+++ src/reports/render_exploratory.R
@@ -7,11 +7,42 @@ render_params = list(train_year = 2020,
 # outcomes
 rmarkdown::render(input = here::here("notebooks", "outcomes.Rmd"),
                   params = render_params,
+                  clean = T,
+                  output_dir = here::here("reports", "exploratory"))
+
+# descriptions
+# rmarkdown::render(input = here::here("notebooks", "descriptions.Rmd"),
+#                   params = render_params,
+#                   output_dir = here::here("reports", "exploratory"))
+
+# designers
+rmarkdown::render(input = here::here("notebooks", "designers.Rmd"),
+                  params = render_params,
+                  clean =T,
+                  output_dir = here::here("reports", "exploratory"))
+
+# publishers
+rmarkdown::render(input = here::here("notebooks", "publishers.Rmd"),
+                  params = render_params,
+                  clean =T,
+                  output_dir = here::here("reports", "exploratory"))
+
+# categories
+rmarkdown::render(input = here::here("notebooks", "categories.Rmd"),
+                  params = render_params,
+                  clean =T,
+                  output_dir = here::here("reports", "exploratory"))
+
+# families
+rmarkdown::render(input = here::here("notebooks", "families.Rmd"),
+                  params = render_params,
+                  clean = T,
                   output_dir = here::here("reports", "exploratory"))
 
 # mechanics
 rmarkdown::render(input = here::here("notebooks", "mechanics.Rmd"),
                   params = render_params,
+                  clean = T,
                   output_dir = here::here("reports", "exploratory"))
 
 
